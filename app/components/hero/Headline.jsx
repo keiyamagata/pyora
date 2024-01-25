dfceeb61047f7e4ac0846a697c47b129c8102439
keiyamagata <kei.yamagata@icloud.com>
2024-01-25 17:12:12 +0100

    feat: add some basic animation

--- app/components/hero/Headline.jsx
+++ app/components/hero/Headline.jsx
@@ -1,19 +1,41 @@
 "use client";
 
+import { useRef } from "react";
 import Link from "next/link";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Headline = () => {
+  const leaseRef = useRef();
+  const titleRef = useRef();
+  const ctaRef = useRef();
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+
+      tl.fromTo(leaseRef.current, { y: 20 }, { y: 0, opacity: 1 });
+      tl.fromTo(titleRef.current, { y: 20 }, { y: 0, opacity: 1 });
+      tl.fromTo(ctaRef.current, {}, { opacity: 1 });
+    },
+    { scope: titleRef }
+  );
+
   return (
     <div className="text-center text-white w-fit">
-      <p className="block mb-2 text-xs uppercase md:text-small">
+      <p
+        ref={leaseRef}
+        className="block mb-2 text-xs uppercase opacity-0 md:text-small"
+      >
         Lease your bike
       </p>
-      <h2 className="text-3xl mb-14 md:text-title">
+      <h2 ref={titleRef} className="text-3xl opacity-0 mb-14 md:text-title">
         Commute with ease.<span className="block">Your company bike.</span>
       </h2>
       <Link
+        ref={ctaRef}
         href="/calculator"
-        className="px-6 py-3 text-sm uppercase transition bg-black rounded-full hover:bg-gray-700"
+        className="px-6 py-3 text-sm uppercase transition bg-black rounded-full opacity-0 hover:bg-gray-700"
       >
         Calculate your price
       </Link>
--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -1,14 +1,33 @@
+"use client";
+
 import Image from "next/image";
 import Headline from "./Headline";
 import RadialGradient from "./RadialGradient";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 const Hero = () => {
+  const imageRef = useRef();
+
+  useGSAP(
+    () => {
+      gsap.fromTo(
+        imageRef.current,
+        { scale: 0.8 },
+        { scale: 1, ease: "circ.out", duration: 0.85, delay: 1.2 }
+      );
+    },
+    { scope: imageRef }
+  );
+
   return (
     <section className="relative h-[100dvh] overflow-hidden flex justify-center">
       <div className="z-30 mt-32">
         <Headline />
       </div>
       <div
+        ref={imageRef}
         className="
       z-20 absolute -bottom-[5rem] left-[50%] -translate-x-1/2 w-[500px] aspect-[1640/908]
       sm:-bottom-[10rem]
