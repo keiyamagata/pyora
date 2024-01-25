659dd9add20410727e217de5b4d54017645c29cb
keiyamagata <kei.yamagata@icloud.com>
2024-01-25 19:16:52 +0100

    update readme

--- app/components/LogoBlack.jsx
+++ app/components/LogoBlack.jsx
@@ -1,8 +1,9 @@
-const LogoBlack = () => {
+const LogoBlack = ({ className }) => {
   return (
     <svg
-      width="124"
-      height="32"
+      className={className || "w-[124px] h-[32px]"}
+      // width="124"
+      // height="32"
       viewBox="0 0 124 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
---
+++ app/loading-animation/page.js
@@ -0,0 +1,12 @@
+"use client ";
+import Loader from "../components/Loader";
+
+const LoadingAnimation = () => {
+  return (
+    <div className="bg-primary">
+      <Loader />
+    </div>
+  );
+};
+
+export default LoadingAnimation;
